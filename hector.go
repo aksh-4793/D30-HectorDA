924b0003e7e47bcae2b219c3b95d09a8b7932edd
Akshay Suresh <akshay.suresh@unotechsoft.com>
2017-01-31 06:08:17 +0000

    Added base folder structure with travis

---
+++ hector.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+*
+	Name : Akshay Suresh
+	Date : 30-01-2017
+	Module : Main
+
+*
+*/
+
+import(
+	"fmt"
+	"./utils"
+)
+
+
+// main
+func main() {
+	fmt.Println("Hector")
+	utils.Server()
+}
